fb0fd5d0c387467423dd6f2414325f9d8abc010d
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-11-21 11:12:42 +0545

    Rich Text

---
+++ lib/screen/rich_text_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class RichTextScreen extends StatelessWidget {
+  const RichTextScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My App"),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: const Text(
+              "Hello World",
+
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          Container(
+            child: const Text(
+              "My Name is Diwas",
+
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          RichText(text: const TextSpan(
+            text: "H",
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 50
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: "ello",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black
+                )
+              ),
+              TextSpan(
+                text: "bold",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
+                  fontSize: 30
+                ),
+              ),
+              TextSpan(
+                text:"world",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black
+                )
+              )
+            ]
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
