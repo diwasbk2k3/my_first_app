1cf5a12a25fd0de991b15b8461a0d8222b573be4
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-09 10:24:03 +0545

    Fixed BottomNaviationBarType

--- lib/screens/bottom_layout_screen.dart
+++ lib/screens/bottom_layout_screen.dart
@@ -8,13 +8,12 @@ class BottomLayoutScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("Diwas")),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag),
-            label: "Cart",
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag),label: "Cart",),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: "Dashboard")
         ],
       ),
     );
