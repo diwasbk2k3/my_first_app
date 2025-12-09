3d22cf5da67d914c26d172e3785ecd45ff9c854d
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-09 10:34:40 +0545

    Changed BottomLayoutScreen from StatelessWidget to StatefulWidget so setState() works properly

--- lib/screens/bottom_layout_screen.dart
+++ lib/screens/bottom_layout_screen.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/screens/bottom_screen.dart/about_screen.dart';
+import 'package:my_first_app/screens/bottom_screen.dart/cart_screen.dart';
+import 'package:my_first_app/screens/bottom_screen.dart/dashboard_screen.dart';
+import 'package:my_first_app/screens/bottom_screen.dart/home_screen.dart';
+import 'package:my_first_app/screens/bottom_screen.dart/profile_screen.dart';
 
-class BottomLayoutScreen extends StatelessWidget {
+int _selectedIndex = 0;
+List<Widget> lstBottomScreen = [
+    const HomeScreen(),
+    const CartScreen(),
+    const ProfileScreen(),
+    const AboutScreen(),
+    const DashboardScreen()
+];
+
+class BottomLayoutScreen extends StatefulWidget {
   const BottomLayoutScreen({super.key});
 
+  @override
+  State<BottomLayoutScreen> createState() => _BottomLayoutScreenState();
+}
+
+class _BottomLayoutScreenState extends State<BottomLayoutScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Diwas")),
+      body: lstBottomScreen[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_bag),label: "Cart",),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+              BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
           BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: "Dashboard")
         ],
+        currentIndex: _selectedIndex,
+        onTap: (index) => {
+          setState((){
+            _selectedIndex = index;
+          })
+        },
       ),
     );
   }
