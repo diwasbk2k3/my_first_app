a94b11b7290f89fd1b66991367cea23bd5798c73
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-12 11:20:56 +0545

    Elevated Button Theme

--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -4,6 +4,20 @@ ThemeData getApplicationTheme(){
   return ThemeData(
     primarySwatch: Colors.orange,
     scaffoldBackgroundColor: Colors.grey[200],
-    fontFamily: "OpenSans Bold"
+    fontFamily: "OpenSans Bold",
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(
+          fontSize: 18,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontFamily: "OpenSans Regular"
+        ),
+        backgroundColor: Colors.orange,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(5)
+        )
+      )
+    )
   );
 }
