f30e176a2ecee29955da03f1d3b509bfd655a0ee
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-12 10:49:32 +0545

    Bottom Layout Screen

--- lib/screens/bottom_layout_screen.dart
+++ lib/screens/bottom_layout_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_app/screens/bottom_screen.dart/about_screen.dart';
-import 'package:my_first_app/screens/bottom_screen.dart/cart_screen.dart';
-import 'package:my_first_app/screens/bottom_screen.dart/dashboard_screen.dart';
-import 'package:my_first_app/screens/bottom_screen.dart/home_screen.dart';
-import 'package:my_first_app/screens/bottom_screen.dart/profile_screen.dart';
+import 'package:my_first_app/screens/bottom_screen/about_screen.dart';
+import 'package:my_first_app/screens/bottom_screen/cart_screen.dart';
+import 'package:my_first_app/screens/bottom_screen/dashboard_screen.dart';
+import 'package:my_first_app/screens/bottom_screen/home_screen.dart';
+import 'package:my_first_app/screens/bottom_screen/profile_screen.dart';
 
 int _selectedIndex = 0;
 List<Widget> lstBottomScreen = [
@@ -25,15 +25,16 @@ class _BottomLayoutScreenState extends State<BottomLayoutScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Diwas")),
+      appBar: AppBar(title: Text("Diwas"), backgroundColor: Colors.lightBlue,),
       body: lstBottomScreen[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.lightBlueAccent,
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag),label: "Cart",),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag),label: "Cart"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-              BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
           BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: "Dashboard")
         ],
         currentIndex: _selectedIndex,
